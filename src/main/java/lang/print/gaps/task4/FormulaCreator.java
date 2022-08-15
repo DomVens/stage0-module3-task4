27686b27b952211ee4bde1dfdedc986f4f7bf2dc
Domantas Venslovas <domantas.venslovas@stud.vilniustech.lt>
2022-08-15 12:33:16 +0300

    hopefully finished task

--- src/main/java/lang/print/gaps/task4/FormulaCreator.java
+++ src/main/java/lang/print/gaps/task4/FormulaCreator.java
@@ -4,6 +4,6 @@ public class FormulaCreator {
     public static void main(String[] args) {
         int a;
         int b;
-        (9*a*a-5*b+2+a-7)*((a+b+4*a*b)/2)
+        //(9*a*a-5*b+2+a-7)*((a+b+4*a*b)/2)
     }
 }
