c406c74ab34f9a3df52852850acd9b3033d4ccb4
Domantas Venslovas <domantas.venslovas@stud.vilniustech.lt>
2022-08-15 12:30:11 +0300

    hopefully finished task

--- src/main/java/lang/print/gaps/task4/IntRepresentation.java
+++ src/main/java/lang/print/gaps/task4/IntRepresentation.java
@@ -2,5 +2,10 @@ package lang.print.gaps.task4;
 
 public class IntRepresentation {
     public static void main(String[] args) {
+        //'c', 'h', 'a', 'r'
+        System.out.println(Character.getNumericValue('c'));
+        System.out.println(Character.getNumericValue('h'));
+        System.out.println(Character.getNumericValue('a'));
+        System.out.println(Character.getNumericValue('r'));
     }
 }
